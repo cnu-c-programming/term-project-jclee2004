f7c80f3b55746e2fe2fc5bc8a191480be1f6380c
jclee04 <knight4765@gmail.com>
2026-05-19 16:39:35 +0900

    권장 파일 구조 구축 및 main 파일 for문 주석 해제 및 csv_path default 값 해제,
    csv_path는 반드시 입력에 포함되도록 변경

--- main.c
+++ main.c
@@ -23,6 +23,10 @@
 /* #include "file_io.h"  */
 /* #include "command.h"  */
 
+#include "student.h"
+#include "file_io.h"
+#include "command.h"
+
 /* ---------------------------------------------------------------
  * TODO: Implement the interactive shell loop.
  *   - Print a prompt and read a line from stdin.
@@ -48,7 +52,7 @@ void run_command_file(const char *cmd_file, const char *csv_path) {
 }
 
 int main(int argc, char *argv[]) {
-    const char *csv_path  = "students.csv"; /* default CSV file */
+    const char *csv_path  = NULL; // Default 삭제하고 NULL로, 조건에 무조건 파일 쓰고, 없으면 사용법 출력이였음. 
     const char *cmd_file  = NULL;           /* -f <file> argument */
 
     /* TODO: Parse command-line arguments.
@@ -66,10 +70,21 @@ int main(int argc, char *argv[]) {
      *       }
      *   }
      */
-    (void)argc;
-    (void)argv;
+    for (int i = 1; i < argc; i++) {
+        if (strcmp(argv[i], "-f") == 0 && i + 1 < argc) { // i+1..? -> -f 까지만 치고 뒤에 입력안했을 경우의 대비
+            cmd_file = argv[++i]; // -f 뒤에 오면 명령어 파일이니 이를 cmd file 변수로 연결
+        } else {
+            csv_path = argv[i]; // 파일 이름이 다를수도 있으니, 일단 기본으로 student.csv 받고, 혹시 모르니 재할당
+        }
+    }
+    if(csv_path == NULL) {
+        printf("Usage: %s <csv_file> [-f command_file]\n",argv[0]);
+        return 1;
+    }
+    
+    
 
-#ifdef ADMIN_MODE
+#ifdef ADMIN_MODE // 전처리기 문법인데, 2주 뒤에나 배움. 맨 위 컴파일 옵션에 따라 분기
     /* Admin shell: supports add, delete, update, save, load, sort, list, find, help, exit */
     if (cmd_file) {
         run_command_file(cmd_file, csv_path);
