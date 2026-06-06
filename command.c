9683862a2d901446265e36ce050a845c15df1b36
jclee04 <knight4765@gmail.com>
2026-06-06 14:47:54 +0900

    코드 기본 명령어 틀 완성 및 컴파일 단계, 성공시 PDF 양식 맞춰 출력 수정 및 남은 함수 추가
    main 수정 요함

--- command.c
+++ command.c
@@ -1,2 +1,85 @@
-// 일단 필요할거 같아서 만들기는 했는데
-// main으로 통합하든 혹시 전처리 매크로 공부하면서 다시 확인
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "command.h"
+#include "file_io.h"
+
+//우선 만들어놓은 간단한 명령어만 구성해서
+//큰 뼈대를 잡고 나머지 명령어 처리
+
+void cmd_add(Student ** head, char *args) {
+    int id,score;
+    char name[100];
+
+    sscanf(args,"%d %s %d",&id,name,&score);
+    add(head,id,name,score);
+}
+
+void cmd_delete(Student ** head, char * args) {
+    int id=atoi(args);
+    delete(head,id);
+}
+
+void cmd_update(Student ** head, char * args) {
+    int id,score;
+    char name[100];
+
+    sscanf(args,"%d %s %d",&id,name,&score);
+    update(head,id,score);
+}
+
+void cmd_find(Student ** head, char * args) {
+    int id = atoi(args);
+    Student * student = find(head,id);
+    if(student != NULL) {
+        printf("%d %s %d",student->id,student->name,student->score);
+    }
+    else {
+        printf("No student");
+    }
+}
+
+
+void cmd_save(Student ** head, char * args) {
+    save(*head,args);
+}
+
+void cmd_load(Student ** head, char * args) {
+    load(head,args);
+}
+
+typedef void (*cmd)(Student ** head,char * args);
+
+typedef struct 
+{
+    char *name;
+    cmd function;
+}CMD;
+
+CMD table[] = {
+    {"add",cmd_add},
+    {"delete",cmd_delete},
+    {"update",cmd_update},
+    {"find",cmd_find},
+    {"save",cmd_save},
+    {"load",cmd_load},
+};
+
+void cmd_process(Student **head, char * input) {
+    char * cmd_name = strtok(input," ");
+    char * args = strtok(NULL,"");
+
+    if (cmd_name == NULL) {
+        printf("명령어를 포함하여 다시 입력하십시오.\n");
+        return;
+    } 
+
+    for(int i=0;i<sizeof(table)/sizeof(table[0]);i++) {
+        if(strcmp(cmd_name,table[i].name)==0) {
+            table[i].function(head,args);
+            return;
+        }
+    }
+    printf("알 수 없는 명령어 : %s",cmd_name);
+
+}
